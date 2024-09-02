56411fcb187429f4f481dbbe147fb880ba270b0f
Arslan Mushtaq <arslanark@gmail.com>
2024-09-03 01:52:31 +0400

    feat: complete project according to article

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
+    res.render('index', {
+        title: 'Using View Engine in Express', message: "This is received from the Server"
+    });
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'));
