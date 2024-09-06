2bbc1f44f98bf187b2868126df0b92e0013b89b1
Arslan Mushtaq <arslanark@gmail.com>
2024-09-06 19:02:34 +0400

    feat: complete project according to article

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
+    res.send('<h1>Hello from Nodemon!</h1>');
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'));
